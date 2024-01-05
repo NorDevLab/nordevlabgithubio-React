0cad8fc058e04833f1ad51f22d419553c4866db1
NorDevLab <155392901+NorDevLab@users.noreply.github.com>
2024-01-05 22:15:29 +0100

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import {
 
 import './style.css'
 import Home from './views/home'
-import Test from './views/test'
 import About from './views/about'
 import NotFound from './views/not-found'
 
@@ -18,7 +17,6 @@ const App = () => {
     <Router>
       <Switch>
         <Route component={Home} exact path="/" />
-        <Route component={Test} exact path="/test" />
         <Route component={About} exact path="/about" />
         <Route component={NotFound} path="**" />
         <Redirect to="**" />
