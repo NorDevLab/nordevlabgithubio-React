acd17893ffc389c51a12ac9932eaee9885f31488
NorDevLab <155392901+NorDevLab@users.noreply.github.com>
2024-01-05 19:09:30 +0100

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -15,6 +15,11 @@ const Home = (props) => {
       </Helmet>
       <div className="home-container1 back">
         <header data-thq="thq-navbar" className="home-navbar-interactive">
+          <img
+            alt="logo"
+            src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
+            className="home-image"
+          />
           <div data-thq="thq-navbar-nav" className="home-desktop-menu">
             <nav className="home-links">
               <span>About</span>
@@ -29,35 +34,37 @@ const Home = (props) => {
             </div>
           </div>
           <div data-thq="thq-burger-menu" className="home-burger-menu">
-            <svg id="nb_icon" viewBox="0 0 1024 1024" className="home-icon">
+            <svg viewBox="0 0 1024 1024" className="home-icon">
               <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
             </svg>
           </div>
           <div data-thq="thq-mobile-menu" className="home-mobile-menu">
             <div className="home-nav">
               <div className="home-top">
+                <img
+                  alt="image"
+                  src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
+                  className="home-logo"
+                />
                 <div data-thq="thq-close-menu" className="home-close-menu">
-                  <svg
-                    id="nb_icon_close"
-                    viewBox="0 0 1024 1024"
-                    className="home-icon02"
-                  >
+                  <svg viewBox="0 0 1024 1024" className="home-icon02">
                     <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
                   </svg>
                 </div>
               </div>
               <nav className="home-links1">
-                <Link to="/about" className="home-navlink">
-                  About
-                </Link>
-                <span className="home-text05">Features</span>
-                <span className="home-text06">Pricing</span>
-                <span className="home-text07">Team</span>
-                <span className="home-text08">Blog</span>
+                <span className="home-text05">About</span>
+                <span className="home-text06">Features</span>
+                <span className="home-text07">Pricing</span>
+                <span className="home-text08">Team</span>
+                <span className="home-text09">Blog</span>
               </nav>
-              <div className="home-buttons1"></div>
+              <div className="home-buttons1">
+                <button className="home-login1 button">Login</button>
+                <button className="button">Register</button>
+              </div>
             </div>
-            <div className="home-icon-group">
+            <div>
               <svg viewBox="0 0 950.8571428571428 1024" className="home-icon04">
                 <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
               </svg>
@@ -69,9 +76,6 @@ const Home = (props) => {
               </svg>
             </div>
           </div>
-          <Link to="/" id="logo_text" className="home-text09">
-            NORDEVLAB
-          </Link>
         </header>
         <div className="home-container2">
           <div id="tblock" className="home-container3">
